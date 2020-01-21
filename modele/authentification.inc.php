40fc903ca1385f8221c92a1650360b36f7df8a7f
Franck Meinhardt <franck.meinhardt@gmail.com>
2020-01-21 13:20:27 +0100

    tout ce qui est connexion

--- modele/authentification.inc.php
+++ modele/authentification.inc.php
@@ -10,9 +10,19 @@ function login($mail, $mdp){
     
     $util = getUtilisateurByMail($mail);
     $mdpBD = $util["mdp"];
+    $grainDeSel = $util["graindesel"];
+    
+    // lors création utilisateurt, utiliser cette fomule pour créer son grain de sel et l'insérer dans table utilisateurs
+    //Grain de sel = ID unique al$grainDeSel = rand(time(),strlen(time()));éatoire, ici du  basé sur le temps (11 octets) :
+    // $grainDeSel = rand(time(),strlen(time()));
+    
+
+    
+    //Hachage simple avec le grain de sel :
+    $hash_v1 = md5($mdpBD.$grainDeSel);
     
     //voir si le mdp est le même que celui de l'utilisateur dans la base de donnée
-    if(trim($mdpBD)== trim(crypt($mdp, $mdpBD))){
+    if(trim($hash_v1)== trim(md5($mdp.$grainDeSel))){
         // trim — Supprime les espaces (ou d'autres caractères) en début et fin de chaîne
         // crypt — Hachage à sens unique (indéchiffrable)
         $_SESSION["mail"] = $mail;
