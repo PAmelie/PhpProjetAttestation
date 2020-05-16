dcf678e611c0ac40c9cc659a53ea55205ac51eb1
PAmelie <apauchard56@gmail.com>
2020-05-16 16:25:13 +0200

    modifier

--- controleur/modifier.php
+++ controleur/modifier.php
@@ -1,8 +1,7 @@
 <?php
 
-/* fonctions ajouterUtilisaeur ModifU, standbyeformation etc
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$titrePage = "TestUtilisateur";
+include "./vue/entete.html.php";
+include "./vue/tableau.php"
 
+?>
